f06ce59f01987c226d3fc71b80b3c4296453fff7
21MoududAhammed <21moudud@gmail.com>
2024-09-11 19:45:59 +0600

    Banner added

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,8 +1,10 @@
 import {Outlet} from 'react-router-dom';
 import Footer from '../shared/Footer';
+import Nav from '../shared/Nav';
 export default function Layout(){
     return (
         <div>
+            <Nav/>
             <Outlet/>
             <Footer/>
         </div>
--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,7 +1,9 @@
-export default function Home(){
-    return (
-        <div>
-            <h1>Home page</h1>
-        </div>
-    );
+import Banner from "./Banner";
+
+export default function Home() {
+  return (
+    <div>
+        <Banner/>
+    </div>
+  );
 }
