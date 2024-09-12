459ceb2fa302c90f7464b6a0d65234e95a8ee47d
21MoududAhammed <21moudud@gmail.com>
2024-09-12 19:31:40 +0600

    Correction with spacing at home page

--- src/components/pages/home/Categories.jsx
+++ src/components/pages/home/Categories.jsx
@@ -9,87 +9,87 @@ import slider2 from "../../../assets/home/slide2.jpg";
 import slider3 from "../../../assets/home/slide3.jpg";
 import SectionHeading from "../../shared/SectionHeading";
 const subHeading = "---From 11:00am to 10:00pm---";
-const heading = 'ORDER ONLINE';
+const heading = "ORDER ONLINE";
 
 export default function Categories() {
   return (
-    <section className='mt-20'>
-        <SectionHeading subHeading={subHeading} heading={heading}/>
-    <Swiper
-      slidesPerView={2}
-      spaceBetween={5}
-      pagination={{
-        clickable: true,
-      }}
-      breakpoints={{
-        // When the window width is >= 640px, show 1 slide
-        640: {
-          slidesPerView: 3,
-          spaceBetween: 20,
-        },
-        // When the window width is >= 768px, show 2 slides
-        768: {
-          slidesPerView: 3,
-          spaceBetween: 30,
-        },
-        // When the window width is >= 1024px, show 3 slides
-        1024: {
-          slidesPerView: 5,
-          spaceBetween: 40,
-        },
-      }}
-      modules={[Pagination]}
-      className="mySwiper mt-14 mb-20 font-cinzel text-white"
-    >
-      <SwiperSlide>
-        <div className="relative">
-          <img src={slider1} alt="" />
-          <h1 className="absolute bottom-5 right-1/3 uppercase text-3xl  z-10">
-            Salads
-          </h1>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="relative">
-          <img src={slider2} alt="" />
-          <h1 className="absolute bottom-5 right-1/3 uppercase text-3xl  z-10">
-            Pizzas
-          </h1>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="relative">
-          <img src={slider3} alt="" />
-          <h1 className="absolute bottom-5 right-1/3 uppercase text-3xl  z-10">
-            Soups
-          </h1>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="relative">
-          <img src={slider1} alt="" />
-          <h1 className="absolute bottom-5 right-1/3 uppercase text-3xl  z-10">
-            Salads
-          </h1>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="relative">
-          <img src={slider2} alt="" />
-          <h1 className="absolute bottom-5 right-1/3 uppercase text-3xl  z-10">
-            Pizzas
-          </h1>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="relative">
-          <img src={slider3} alt="" />
-          <h1 className="absolute bottom-5 right-1/3 uppercase text-3xl  z-10">
-            Soups
-          </h1>
-        </div>
-      </SwiperSlide>
-    </Swiper>
+    <section className="my-10 sm:my-16 md:my-20">
+      <SectionHeading subHeading={subHeading} heading={heading} />
+      <Swiper
+        slidesPerView={2}
+        spaceBetween={5}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          // When the window width is >= 640px, show 1 slide
+          640: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+          // When the window width is >= 768px, show 2 slides
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+          // When the window width is >= 1024px, show 3 slides
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 40,
+          },
+        }}
+        modules={[Pagination]}
+        className="mySwiper mt-8 sm:mt-10 md:mt-16 font-cinzel text-white"
+      >
+        <SwiperSlide>
+          <div className="relative">
+            <img src={slider1} alt="" />
+            <h1 className="absolute bottom-5 right-1/3 uppercase text-3xl  z-10">
+              Salads
+            </h1>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative">
+            <img src={slider2} alt="" />
+            <h1 className="absolute bottom-5 right-1/3 uppercase text-3xl  z-10">
+              Pizzas
+            </h1>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative">
+            <img src={slider3} alt="" />
+            <h1 className="absolute bottom-5 right-1/3 uppercase text-3xl  z-10">
+              Soups
+            </h1>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative">
+            <img src={slider1} alt="" />
+            <h1 className="absolute bottom-5 right-1/3 uppercase text-3xl  z-10">
+              Salads
+            </h1>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative">
+            <img src={slider2} alt="" />
+            <h1 className="absolute bottom-5 right-1/3 uppercase text-3xl  z-10">
+              Pizzas
+            </h1>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="relative">
+            <img src={slider3} alt="" />
+            <h1 className="absolute bottom-5 right-1/3 uppercase text-3xl  z-10">
+              Soups
+            </h1>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </section>
   );
 }
--- src/components/pages/home/Details.jsx
+++ src/components/pages/home/Details.jsx
@@ -2,13 +2,13 @@ import cefService from "../../../assets/home/chef-service.jpg";
 export default function Details() {
   return (
     <section
-      className="bg-cover bg-center h-fit w-full flex justify-center items-center text-center mb-20"
+      className="bg-cover bg-center h-fit w-full flex justify-center items-center text-center mb-10 sm:mb-16 md:mb-20"
       style={{ backgroundImage: `url(${cefService})` }}
     >
-      <div className="p-5 md:p-20">
-        <div className="bg-white p-5 md:p-20 space-y-5">
-          <h1 className="font-cinzel uppercase text-5xl">{`Faria's Restaurant`}</h1>
-          <p className="font-inter text-base">
+      <div className="p-2 sm:p-5 md:p-20">
+        <div className="bg-white p-3 sm:p-5 md:p-20 space-y-2 sm:space-y-3 md:space-y-5">
+          <h1 className="font-cinzel uppercase text-2xl sm:text-3xl md:text-5xl">{`Faria's Restaurant`}</h1>
+          <p className="font-inter text-xs sm:text-sm md:text-base">
             {` Welcome to Faria's Restaurant, where classic flavors meet modern
           creativity! Located in the heart of the city, we serve fresh, locally
           sourced ingredients in every dish. Whether you're here for a casual
