45a8a37c2869111f761459641d26d8945a873ad0
21MoududAhammed <21moudud@gmail.com>
2024-09-14 14:39:32 +0600

    Banner added in our menu page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Home from "./components/pages/home/Home";
+import OurMenu from "./components/pages/ourMenu/OurMenu";
 export default function App() {
   return (
     <div className="font-inter max-w-screen-2xl mx-auto">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/our-menu" element={<OurMenu />} />
         </Route>
       </Routes>
     </div>
---
+++ src/components/pages/ourMenu/OurMenu.jsx
@@ -0,0 +1,9 @@
+import Banner from "./Banner";
+
+export default function OurMenu(){
+    return (
+        <div>
+            <Banner/>
+        </div>
+    );
+}
